0b28362f6d59fd02324094259b15ae541c96363b
Abhay Sharma <abhyaas7@gmail.com>
2026-05-12 11:31:50 +0530

    Add links

--- src/LandingPage/ForgotPassword/Hero.js
+++ src/LandingPage/ForgotPassword/Hero.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const backendUrl = "https://findbuddy-back.onrender.com";
+const backendUrl = "https://find-buddy-backend.vercel.app";
 
 function Hero() {
     const [email, setEmail] = useState('');
--- src/LandingPage/ResetPassword/Hero.js
+++ src/LandingPage/ResetPassword/Hero.js
@@ -9,7 +9,7 @@ function ResetPassword() {
     const [confirmPassword, setConfirmPassword] = useState('');
     const [loading, setLoading] = useState(false);
 
-    const backendUrl = "https://findbuddy-back.onrender.com";
+    const backendUrl = "https://find-buddy-backend.vercel.app";
 
     const navigate = useNavigate();
     const token = searchParams.get('token');
